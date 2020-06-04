551cf1ebde9c7c3e6bfb106f8255d29b53a5d482
mdsagir <tech.sagir@gmail.com>
2020-06-04 11:51:41 +0530

    added project

--- jenking-integeration/src/main/java/com/greenjava/resource/HomeController.java
+++ jenking-integeration/src/main/java/com/greenjava/resource/HomeController.java
@@ -1,4 +1,13 @@
 package com.greenjava.resource;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+
+    @GetMapping("hello")
+    public String home(){
+        return "Hello";
+    }
 }
---
+++ jenking-integeration/src/test/java/com/greenjava/JenkingIntegerationApplicationTests.java
@@ -0,0 +1,12 @@
+package com.greenjava;
+
+
+
+
+class JenkingIntegerationApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
